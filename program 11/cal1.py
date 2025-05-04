e6d4dc93ef414298b0bc436646deb80cc34eef2c
Urja Chandra <urjachandra9th@gmail.com>
2025-05-04 11:54:53 +0530

    Add files via upload

---
+++ program 11/cal1.py
@@ -0,0 +1,17 @@
+def add1(x,y):
+    print(x+y)
+
+def sub1(x,y):
+    print(x-y)
+
+def mul1(x,y):
+    print(x*y)
+
+def div1(x,y):
+    print(x/y)
+
+def mod1(x,y):
+    print(x%y)
+
+def pow1(x,y):
+    print(x**y)
---
+++ program 11/m5.py
@@ -0,0 +1,8 @@
+import cal1
+
+cal1.add1(3,4)
+cal1.sub1(7,3)
+cal1.mul1(12,8)
+cal1.div1(10,5)
+cal1.mod1(5,2)
+cal1.pow1(2,4)
